be6cb3a7af7dbcfd09e5540d0c95cb7b15fb6706
Large Chonk <hanleyharris96@gmail.com>
2020-05-06 15:25:51 +0100

    enabled CORS on the controller

--- src/main/java/com/bae/kudos/elasticsearch/controller/ElasticController.java
+++ src/main/java/com/bae/kudos/elasticsearch/controller/ElasticController.java
@@ -5,11 +5,15 @@ import com.bae.kudos.elasticsearch.model.Feedback;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
+@RequestMapping("/")
 public class ElasticController {
     
     private  ElasticsearchOperations elasticsearchOperations;
